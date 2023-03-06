7a5e00249cb3f20778a233594cc1819724b56442
Duleeka.Munasinghe <duleekalakm@gmail.com>
2023-03-06 22:12:38 +1300

    changes have been made

---
+++ src/AssistedPractice/Puppy.java
@@ -0,0 +1,14 @@
+package AssistedPractice;
+
+public class Puppy {
+public Puppy(String name) {
+	//This constructor has one parameter, name.
+	System.out.println("Passeed name is :" + name);
+}
+
+public static void main(String []args) {
+	// Following statements would creat an object myPuppy
+	Puppy myPuppy = new Puppy("tommy");
+	
+}
+}
